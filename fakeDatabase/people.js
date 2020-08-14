cb972ea9a992068a4c760a54121a1c5485fb5e49
cawilson1 <cawilson1@g.cofc.edu>
2020-08-14 12:39:25 -0400

    added all people to db

--- fakeDatabase/people.js
+++ fakeDatabase/people.js
@@ -12,5 +12,37 @@ const people = [
     role: "Student",
     wow: "There has never been a sadness that can't be cured by breakfast food",
     github: "https://github.com/Benanna2019"
+  },
+  {
+    name: "West M.",
+    pic: "https://i2.wp.com/www.surfer.com/wp-content/uploads/2019/03/21.jpg",
+    role: "Student",
+    wow: "Keep it simple stupid",
+    github: "https://github.com/WestMoz"
+  },
+  {
+    name: "Mike A.",
+    pic:
+      "https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/golden-retriever-card-small.jpg?bust=1535568541",
+    role: "Student",
+    wow: "If you're not first you're last",
+    github: "https://github.com/mikealessi24"
+  },
+  {
+    name: "Dobson D.",
+    pic:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQuUhMycpY9JTe2xMaTN9vtdvLzBedrofEN-Q&usqp=CAU ",
+    role: "Student",
+    wow: "There is no try, only do",
+    github: " https://github.com/jdobsond"
+  },
+  {
+    name: "Bo F.",
+    pic:
+      "https://mytanfeet.com/wp-content/uploads/2017/12/Rio-Celeste-waterfall-Best-waterfalls-in-Costa-Rica.jpg ",
+    role: "Student",
+    wow:
+      "You don't have to be great to start, but you have to start to be great",
+    github: "https://github.com/rwfarish"
   }
 ];
